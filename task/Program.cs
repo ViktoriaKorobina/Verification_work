1ba7bbceb9804950d2db52ff204e561b9ad16f22
Viktoria <viktoria.korobjina@yandex.ru>
2023-10-21 12:51:23 +0300

    Added resolve

---
+++ task/Program.cs
@@ -0,0 +1,36 @@
+﻿int SizeNewArray(string[] array)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3) size += 1;
+    }
+    return size;
+}
+string[] ArrayOfLengthIsThree(string[] array, int size)
+{
+    string[] newArray = new string[size];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count += 1;
+        }
+    }
+    return newArray;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+string[] firstArray = new string[] {"Hello", "fic", "2", "world", ":-)"};
+ShowArray(firstArray);
+int size = SizeNewArray(firstArray);
+string[] secondArray = ArrayOfLengthIsThree(firstArray, size);
+ShowArray(secondArray);
